e8448d1e86753f2f353b6ffa7be07e813c0f4aa5
Kenio Pereira de Farias <keniopf@gmail.com>
2020-05-25 17:37:25 -0300

    Signed-off-by: Kenio Pereira de Farias <keniopf@gmail.com>

---
+++ index.php
@@ -0,0 +1,122 @@
+<?php 
+session_start();
+?>
+
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+        <script src="js.js"></script>
+        <link href="css.css" rel="stylesheet">
+
+
+    <title>Arquivos Externos</title>
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="alert alert-primary text-center mt-5 text-uppercase" role="alert">
+            Serviço de Envio de arquivos Externos
+        </div>
+
+        <?php
+                if(isset($_SESSION['msg'])){
+                echo $_SESSION['msg'];
+                unset($_SESSION['msg']);
+                }
+            ?>
+
+
+        <form enctype="multipart/form-data" action="upload.php" method="POST">
+
+
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <label class="input-group-text" for="inputGroupSelect01">Nome</label>
+                </div>
+                <select name="nome" class="custom-select" id="inputGroupSelect01">
+                    <option selected>Escolha o Escrevente...</option>
+                    <option value="Raimundo">Raimundo</option>
+                    <option value="Marcelo">Marcelo</option>
+                </select>
+            </div>
+
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <label class="input-group-text" for="inputGroupSelect01">Setor</label>
+                </div>
+                <select name="setor" class="custom-select" id="inputGroupSelect01">
+                    <option selected>Escolha o Setor...</option>
+                    <option value="proc">Procuração</option>
+                    <option value="escrit">Escritura</option>
+                    <option value="rcivil">Registro Civil</option>
+                    <option value="tit">Titulos</option>
+                    <option value="apt">Apostilamento</option>
+                    <option value="fir">Firma</option>
+                </select>
+            </div>
+
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <button class="input-group-text">Data</button>
+                </div>
+                <input name="livro" type="text" class="form-control" placeholder="Livro - Folha - Protocolo - Registro">
+            </div>
+
+
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <button class="input-group-text">Data</button>
+                </div>
+                <input name="data" type="date" class="form-control" placeholder=""
+                    aria-label="Example text with button addon" aria-describedby="button-addon1">
+            </div>
+
+
+
+            <div class="input-group mt-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="inputGroupFileAddon01">Upload</span>
+                </div>
+                <div class="custom-file">
+                    <input name="arquivo" type="file" class="custom-file-input" id="inputGroupFile01"
+                        aria-describedby="inputGroupFileAddon01">
+                    <label class="custom-file-label" for="inputGroupFile01">Envie seu arquivo...</label>
+                </div>
+            </div>
+            <button class="submit btn btn-primary btn-block mt-3">Enviar</button>
+        </form>
+
+        <div id="myProgress">
+            <div id="myBar"></div>
+        </div>
+
+
+    </div>
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
---
+++ upload.php
@@ -0,0 +1,103 @@
+<?php
+session_start();
+
+
+$nome = $_POST['nome'];
+$setor = $_POST['setor'];
+$data = $_POST['data'];
+$livro = $_POST['livro'];
+ 
+// Pasta onde o arquivo vai ser salvo
+
+ $_UP['pasta'] = 'upload/'.$_POST['nome'].'/';
+
+ print_r($_UP);
+
+// switch($_POST['nome']){
+//     case 'Raimundo':
+//         $_UP['pasta'] = 'upload/Raimundo/';
+//      break;
+
+//  case 'Marcelo':
+//         $_UP['pasta'] = 'upload/Marcelo/';
+//      break;
+//     }
+
+//     print_r($_POST);
+
+
+
+ 
+// Tamanho máximo do arquivo (em Bytes)
+$_UP['tamanho'] = 1024 * 1024 * 6; // 2Mb
+ 
+// Array com as extensões permitidas
+$_UP['extensoes'] = array('jpg', 'png', 'gif', 'jpeg', 'pdf', 'mp4');
+ 
+// Renomeia o arquivo? (Se true, o arquivo será salvo como .jpg e um nome único)
+$_UP['renomeia'] = false;
+ 
+// Array com os tipos de erros de upload do PHP
+$_UP['erros'][0] = 'Não houve erro';
+$_UP['erros'][1] = 'O arquivo no upload é maior do que o limite do PHP';
+$_UP['erros'][2] = 'O arquivo ultrapassa o limite de tamanho especifiado no HTML';
+$_UP['erros'][3] = 'O upload do arquivo foi feito parcialmente';
+$_UP['erros'][4] = 'Não foi feito o upload do arquivo';
+ 
+// Verifica se houve algum erro com o upload. Se sim, exibe a mensagem do erro
+if ($_FILES['arquivo']['error'] != 0) {
+die("Não foi possível fazer o upload, erro:<br />" . $_UP['erros'][$_FILES['arquivo']['error']]);
+exit; // Para a execução do script
+}
+ 
+// Caso script chegue a esse ponto, não houve erro com o upload e o PHP pode continuar
+ 
+// Faz a verificação da extensão do arquivo
+$extensao = strtolower(end(explode('.', $_FILES['arquivo']['name'])));
+if (array_search($extensao, $_UP['extensoes']) === false) {
+// echo "Por favor, envie arquivos com as seguintes extensões: jpg, png ou gif";
+$_SESSION['msg'] = "<div class='alert alert-danger text-center'>Por favor, envie arquivos com as seguintes extensões: jpg, jpeg, png, gif, pdf ou mp4";
+header("Location: index.php");
+
+}
+ 
+// Faz a verificação do tamanho do arquivo
+else if ($_UP['tamanho'] < $_FILES['arquivo']['size']) {
+// echo "O arquivo enviado é muito grande!";
+$_SESSION['msg'] = "<div class='alert alert-danger text-center'>O arquivo enviado é muito grande!";
+header("Location: index.php");
+}
+ 
+// O arquivo passou em todas as verificações, hora de tentar movê-lo para a pasta
+else {
+// Primeiro verifica se deve trocar o nome do arquivo
+if ($_UP['renomeia'] == true) {
+// Cria um nome baseado no UNIX TIMESTAMP atual e com extensão .jpg
+$nome_final = $livro." - ".$nome." - ".$setor." - ".$data.".".$extensao;
+} else {
+// Mantém o nome original do arquivo
+//$nome_final = $_FILES['arquivo']['name'];
+$nome_final = $livro." - ".$nome." - ".$setor." - ".$data.".".$extensao;
+}
+ 
+// Depois verifica se é possível mover o arquivo para a pasta escolhida
+if (move_uploaded_file($_FILES['arquivo']['tmp_name'], $_UP['pasta'] . $nome_final)) {
+// Upload efetuado com sucesso, exibe uma mensagem e um link para o arquivo
+// echo "Upload efetuado com sucesso!";
+// echo '<br /><a href="' . $_UP['pasta'] . $nome_final . '">Clique aqui para acessar o arquivo</a>';
+
+$_SESSION['msg'] = "<div class='alert alert-success text-center'>Arquivo Enviado com Sucesso</div>";
+header("Location: index.php");
+} else {
+// Não foi possível fazer o upload, provavelmente a pasta está incorreta
+// echo "Não foi possível enviar o arquivo, tente novamente";
+// var_dump($_UP);
+$_SESSION['msg'] = "<div class='alert alert-danger text-center'>Não foi possível enviar o arquivo, tente novamente";
+header("Location: index.php");
+}
+ 
+}
+ 
+?>
+
+
